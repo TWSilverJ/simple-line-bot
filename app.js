008c0a32d4709734f5c63bc008238e7818618699
SilverJ <jack4254@icloud.com>
2023-09-04 22:44:51 +0800

    feat: 使用 dotenv 套件引用環境變數

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import logger from 'morgan'
+
+// 導入應用程式設定
+dotenv.config()
+const { env } = process
 
 // 實作 express
 const app = express()
@@ -12,6 +17,6 @@ app.get('/', (req, res) => {
 })
 
 // 監聽電腦指定 port
-app.listen(3000, async () => {
+app.listen(Number(env.PORT), async () => {
     console.log(new Date().toISOString(), '伺服器啟動')
 })
